95102c9b10b30ca6f02ac095060a92db13d3ffd8
yejwoo <yejinwoo.me@gmail.com>
2023-08-10 15:26:47 +0900

    class 만들기

--- app.js
+++ app.js
@@ -1,62 +1,164 @@
+// 캔바스, 블록 및 변수 리스트
 const canvas = document.querySelector("canvas");
 const c = canvas.getContext("2d");
+const block = 32;
+let score = 0;
 
+canvas.width = block * 10;
+canvas.height = block * 9;
 
-canvas.width = 32 * 10;
-canvas.height = 32 * 9;
+// draw 클래스
 
-const level1 = new Image();
-level1.src = "img/map/level1.png";
+class Player {
+  constructor(src, cx, cy) {
+    // 파라미터를 가져왔으면
+    this.img = new Image();
+    this.img.src = src;
+    this.img.onload = () => {
+      this.loaded = true;
+      if (this.loaded) this.draw();
+    };
+    // 아래처럼 사용해야 함
+    this.cx = cx;
+    this.cy = cy;
+    this.crop = { x: block * this.cx, y: block * this.cy };
+    this.position = { x: 32, y: 224 };
+  }
+  update() {}
+  draw() {
+    if (this.loaded) {
+      c.drawImage(
+        this.img,
+        this.crop.x,
+        this.crop.y,
+        block,
+        block,
+        this.position.x,
+        this.position.y,
+        block,
+        block
+      );
+    }
+  }
+}
 
-level1.onload = () => {
-  c.drawImage(level1, (canvas.width - 320) / 2, 0);
-};
+class Enemy extends Player {
+  constructor(src, cx, cy) {
+    super(src, cx, cy);
+    this.position = { x: 64, y: 32 };
+  }
+}
 
-const heart = new Image();
-heart.src = "img/heart.png";
+class Donut extends Player {
+  constructor(src, cx, cy) {
+    super(src, cx, cy);
+  }
+  draw() {
+    if (this.loaded) {
+      c.drawImage(this.img, this.crop.x, this.crop.y, block, block);
+    }
+  }
+}
 
-heart.onload = () => {
-  c.drawImage(heart, 0, 0, 48, 16, 8, 264, 48, 16);
-};
+class Stage extends Player {
+  constructor(src) {
+    super(src);
+    this.img = new Image();
+    this.img.src = src;
+    this.img.onload = () => {
+      this.loaded = true;
+      if (this.loaded) this.draw();
+    };
+  }
+  update() {
+    // 점수에 따라 배경 업데이트
+  }
+  draw() {
+    c.drawImage(this.img, 0, 0);
+  }
+}
 
-const donutSmall = new Image();
-donutSmall.src = "img/donutSmall.png";
+class Life extends Player {
+  constructor(src, cx, cy, cw, ch, dx, dy, dw, dh) {
+    super(src, cx, cy);
+    this.cw = cw;
+    this.ch = ch;
+    this.dx = dx;
+    this.dy = dy;
+    this.dw = dw;
+    this.dh = dh;
+  }
+  draw() {
+    if (this.loaded) {
+      c.drawImage(
+        this.img,
+        this.crop.x,
+        this.crop.y,
+        this.cw,
+        this.ch,
+        this.dx,
+        this.dy,
+        this.dw,
+        this.dh
+      );
+    }
+  }
+}
 
-donutSmall.onload = () => {
-  c.drawImage(donutSmall, 264, 264, 16, 16);
-};
+class Score extends Player {
+  constructor(src, dx, dy) {
+    super(src);
+    this.dx = dx;
+    this.dy = dy;
+  }
+  draw() {
+    if (this.loaded) {
+      c.drawImage(this.img, this.dx, this.dy);
+    }
+  }
+}
 
-const donutLarge = new Image();
-donutLarge.src = "img/donutLarge.png";
+class Status extends Player {
+  constructor(src, cx, cy, cw, ch, dx, dy, dw, dh) {
+    super(src, cx, cy);
+    this.cw = cw;
+    this.ch = ch;
+    this.dx = dx;
+    this.dy = dy;
+    this.dw = dw;
+    this.dh = dh;
+  }
+  draw() {
+    if (this.loaded) {
+      c.drawImage(
+        this.img,
+        this.crop.x,
+        this.crop.y,
+        this.cw,
+        this.ch,
+        this.dx,
+        this.dy,
+        this.dw,
+        this.dh
+      );
+    }
+  }
+}
 
-donutLarge.onload = () => {
-  c.drawImage(donutLarge, 5*32, 5*32);
-};
+// 아래에 있는 것(배경) 먼저 선언
+const stage1 = new Stage("img/map/stage1.png");
+const enemy = new Enemy("img/slime.png", 0, 0);
+const player = new Player("img/sprite/luna.png", 1, 3);
+const donut = new Donut("img/donutLarge.png", 5, 5);
+const life = new Life("img/heart.png", 0, 0, 48, 16, 8, 264, 48, 16);
+const donutScore = new Score("img/donutSmall.png", 264, 264);
 
-const slime = new Image();
-slime.src = "img/slime.png";
-
-slime.onload = () => {
-  c.drawImage(slime, 0, 0, 32, 32, 2*32, 1*32, 32, 32);
-};
-
-
-const luna = new Image();
-luna.src = "img/sprite/luna.png";
-
-luna.onload = () => {
-  c.drawImage(luna, 32, 96, 32, 32, 1*32, 7*32, 32, 32);
-};
-
-let donutScore = 0;
-
+//폰트
 const myFont = new FontFace("myFont", "url(Minecraft.ttf)");
 
 myFont.load().then((font) => {
   document.fonts.add(font);
-  console.log("Font loaded");
   c.font = "16px myFont";
   c.fillStyle = "Black";
-  c.fillText(`x ${donutScore}`, 284, 277);
+  c.fillText(`x ${score}`, 284, 277);
 });
-
